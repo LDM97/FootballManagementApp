775946e98b0d5843423384f81b3439b69534afdb
Liam Mortimer <LDM97@users.noreply.github.com>
2018-04-12 22:49:37 +0100

    Team search fully working. User can click on result and will be added to that team.
    Signed-off-by: Liam Mortimer <LDM97@users.noreply.github.com>

--- app/src/main/java/mortimer/l/footballmanagement/TeamSearchResults.java
+++ app/src/main/java/mortimer/l/footballmanagement/TeamSearchResults.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,128 +24,116 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class TeamSearchResults extends AppCompatActivity implements View.OnClickListener
 {
 
-    String query;
-
     private FirebaseAuth auth;
     private NavDrawerHandler navDrawerHandler= new NavDrawerHandler();
     private DrawerLayout navDraw;
+    private final Map<View, String> viewToTeamId = new HashMap<>();
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate( savedInstanceState );
-        setContentView( R.layout.activity_team_search_results );
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_team_search_results);
 
         // Custom toolbar setup
-        Toolbar custToolBar = (Toolbar) findViewById( R.id.my_toolbar );
-        setSupportActionBar( custToolBar );
+        Toolbar custToolBar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(custToolBar);
 
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayShowTitleEnabled( false );
+        actionBar.setDisplayShowTitleEnabled(false);
 
         TextView actionBarTitle = (TextView) findViewById(R.id.toolbarTitle);
-        actionBarTitle.setText( "Search Results" );
+        actionBarTitle.setText("Search Results");
 
-        actionBar.setDisplayHomeAsUpEnabled( true );
-        actionBar.setHomeAsUpIndicator( R.drawable.menu_icon );
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.menu_icon);
         // Setup logout button and home button
-        findViewById( R.id.navLogout ).setOnClickListener( this );
-        findViewById( R.id.homeBtn ).setOnClickListener( this );
+        findViewById(R.id.navLogout).setOnClickListener(this);
+        findViewById(R.id.homeBtn).setOnClickListener(this);
 
         // Get Firebase authenticator
         auth = FirebaseAuth.getInstance();
 
         // Nav drawer code
-        navDraw = findViewById( R.id.drawer_layout );
-        NavigationView navigationView = findViewById( R.id.nav_view );
+        navDraw = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
         // Hide items, no team not allowed to navigate to team info screens
         Menu nav_Menu = navigationView.getMenu();
-        nav_Menu.findItem( R.id.teamCalendar ).setVisible( false );
-        nav_Menu.findItem( R.id.discussionBoard ).setVisible( false );
-        nav_Menu.findItem( R.id.teamInfo ).setVisible( false );
+        nav_Menu.findItem(R.id.teamCalendar).setVisible(false);
+        nav_Menu.findItem(R.id.discussionBoard).setVisible(false);
+        nav_Menu.findItem(R.id.teamInfo).setVisible(false);
 
 
-        final String QUERY;
-        // Display thing query search results
-        //if( getIntent().hasExtra( "mortimer.l.footballmanagement.query" ) )
-        //{
-        QUERY = getIntent().getExtras().getString( "mortimer.l.footballmanagement.query" );
-
-        //}
+        final String QUERY = getIntent().getExtras().getString("mortimer.l.footballmanagement.query");
 
         // Get teams
-        FirebaseDatabase database =  FirebaseDatabase.getInstance();
-        DatabaseReference teamsRef = database.getReference().child( "Teams" );
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference teamsRef = database.getReference().child("Teams");
 
-        teamsRef.addListenerForSingleValueEvent( new ValueEventListener()
-        {
+        teamsRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onDataChange( DataSnapshot snapshot )
-            {
-                Iterable <DataSnapshot> teams = snapshot.getChildren();
+            public void onDataChange(DataSnapshot snapshot) {
+                Iterable<DataSnapshot> teams = snapshot.getChildren();
 
                 // Populate the list of teams
-                List<Team> teamsList = new LinkedList<>();
+                List<Team> matchingTeams = new LinkedList<>();
 
-                for( DataSnapshot team : teams )
-                {
-                    teamsList.add( team.getValue( Team.class ) );
+                for (DataSnapshot team : teams) {
+                    Team teamObj = team.getValue(Team.class);
+
+                    // If team name matches the query, add it to the mathinc teams list
+                    if (teamObj.getTeamName().toLowerCase().contains(QUERY.toLowerCase())) {
+                        matchingTeams.add(teamObj);
+                    }
                 }
 
                 // Set top padding to 80dp so doesn't clash w action bar
                 float scale = getResources().getDisplayMetrics().density;
-                int padding = (int) ( 8*scale + 0.5f );
-                int topPadding = (int) ( 80*scale + 0.5f );
-
-                // Make list of teams that actually match the query
-                List<Team> matchingTeams = new LinkedList<>();
-                for( Team team : teamsList )
-                {
-                    if( team.getTeamName().toLowerCase().contains( QUERY.toLowerCase() ) )
-                    {
-                        matchingTeams.add( team );
-                    }
-                }
+                int padding = (int) (8 * scale + 0.5f);
+                int topPadding = (int) (80 * scale + 0.5f);
 
 
-                if( matchingTeams.isEmpty() )
-                { // No teams match the query
+                if (matchingTeams.isEmpty()) { // No teams match the query, display this to the user
                     TextView noResults = getTextView();
                     noResults.setPadding(padding, topPadding, padding, padding);
-                    noResults.setText( "No matches found for: " + QUERY );
-                }
-                else { // Print out the results
+                    noResults.setText("No matches found for: " + QUERY);
+                } else { // Print out the results
 
                     // Loop through teams and output their name to the screen
                     for (int i = 0; i < matchingTeams.size(); i++) {
 
+                        // Get text view to display the output
+                        TextView resultFrame = getTextView();
+
                         if (i == 0) {
                             // Set padding to not clash w action bar for first item
-                            TextView resultFrame = getTextView();
                             resultFrame.setPadding(padding, topPadding, padding, padding);
-                            resultFrame.setText( matchingTeams.get(i).getTeamName());
+                            resultFrame.setText(matchingTeams.get(i).getTeamName());
                         } else {
                             // Set text to team name
-                            TextView resultFrame = getTextView();
-                            resultFrame.setText( matchingTeams.get(i).getTeamName());
+                            resultFrame.setText(matchingTeams.get(i).getTeamName());
+
                         }
+                        // Map the view to the team
+                        viewToTeamId.put( resultFrame, matchingTeams.get( i ).getTeamId() );
                     }
                 }
 
             }
 
             @Override
-            public void onCancelled( DatabaseError databaseError) {
-                System.out.println( "The read failed: " + databaseError.getCode() );
+            public void onCancelled(DatabaseError databaseError) {
+                System.out.println("The read failed: " + databaseError.getCode());
             }
-        } );
+        });
 
     }
 
@@ -162,6 +151,10 @@ public class TeamSearchResults extends AppCompatActivity implements View.OnClick
         int padding = (int) ( 10*scale + 0.5f );
         tv.setTextColor( Color.BLACK );
         tv.setPadding( padding, padding, padding, padding );
+
+        // Make clickable for user to select this thing
+        tv.setClickable( true );
+        tv.setOnClickListener( this );
 
         // Add the text view to the linear layout container on the page
         LinearLayout linearLayout = (LinearLayout)findViewById( R.id.content_frame );
@@ -186,8 +179,10 @@ public class TeamSearchResults extends AppCompatActivity implements View.OnClick
     }
 
     @Override
-    public boolean onOptionsItemSelected( MenuItem item ) {
-        if ( item.getItemId() == android.R.id.home ) {
+    public boolean onOptionsItemSelected( MenuItem item )
+    {
+        if ( item.getItemId() == android.R.id.home )
+        {
             navDraw.openDrawer( GravityCompat.START );
             return true;
         }
@@ -208,6 +203,51 @@ public class TeamSearchResults extends AppCompatActivity implements View.OnClick
         {
             Intent homeScreenActivity = new Intent( getApplicationContext(), NoTeamHome.class );
             startActivity( homeScreenActivity );
+        }
+
+        if( viewToTeamId.containsKey( v ) )
+        { // If clicked view is one of the team results
+
+            final String teamId = viewToTeamId.get( v );
+            final String userId = auth.getCurrentUser().getUid();
+
+            // Get teams
+            final FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference teamsRef = database.getReference(); //.child( "Teams" ).child( teamId );
+
+            teamsRef.addListenerForSingleValueEvent( new ValueEventListener() {
+                @Override
+                public void onDataChange( DataSnapshot snapshot )
+                {
+                    Team selectedTeam = snapshot.child( "Teams" ).child( teamId ).getValue( Team.class );
+                    User currentUser = snapshot.child( "Users" ).child( userId ).getValue( User.class );
+
+                    // Add user to the team, set this team obj to be the new value for the team
+                    selectedTeam.addPlayer( currentUser );
+                    DatabaseReference teamRef = database.getReference().child( "Teams" ).child( teamId );
+                    teamRef.setValue( selectedTeam );
+
+                    // Remove the user from the default users database as added to players list of a team
+                    DatabaseReference userRef = database.getReference().child( "Users" ).child( userId );
+                    userRef.removeValue();
+
+
+                    // Notify user they have been added to this team
+                    Toast.makeText( getApplicationContext(), "You have been added to team: " + selectedTeam.getTeamName(),
+                            Toast.LENGTH_SHORT).show();
+
+                    // Return user to home screen
+                    Intent homeActivity = new Intent( getApplicationContext(), DefaultHome.class );
+                    startActivity( homeActivity );
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError)
+                {
+                    System.out.println("The read failed: " + databaseError.getCode());
+                }
+            });
+
         }
 
     }
